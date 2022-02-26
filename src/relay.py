310ee31dfbef6d0527f96dd2f65380e5db107dad
Areeb-M <am0247@uah.edu>
2022-02-26 15:55:03 -0600

    Lots of changes

--- src/relay.py
+++ src/relay.py
@@ -5,48 +5,58 @@ import serial
 import time
 import gpiozero
 from arm_relay.msg import JointRateCommand, ActuatorFeedback
-from arm_relay.srv import HomeArmBase, HomeArmBaseResponse, EnableActuators, EnableActuatorsResponse
+from endeffector_controller.srv import EnableServos, EnableServosResponse, Home, HomeResponse
 from std_msgs import *
+import lss
+import lss_const as lssc
 
-class ArmRelay:
+class EndEffectorController:
     def __init__(self):
         rospy.loginfo('Setting up node.')
-        rospy.init_node('arm_base_relay')
-        
-        # Initialize serial port and connect to teensy microcontroller
-        self.ser = None
-        while (self.ser is None):
-            port = rospy.get_param('~arm_teensy_serial_port', '/dev/serial0')
-            try:
-                self.ser = serial.Serial(port,115200)
-            except:
-                rospy.loginfo("Unable to locate Teensy (@{0}), please ensure communications.".format(port))
-                time.sleep(3)
-        rospy.loginfo('Connected to teensy.')
+        rospy.init_node('end_effector_control')
         
         self.status_pub = rospy.Publisher('status', msg.String, queue_size=10)
         self.feedback_pub = rospy.Publisher('feedback', ActuatorFeedback, queue_size=1)
         
         self.cmd_rate_sub = rospy.Subscriber("/joint_rate_command", JointRateCommand, self.process_rate_cmd)
-        
-        self.homing_service = rospy.Service("/home_arm_base", HomeArmBase, self.home_arm_base)
+
+        self.enable_service = rospy.Service("/enable_servos", EnableServos, self.enable_servos)
+        self.actuators_enabled = True
+        self.enable_pin = gpiozero.OutputDevice(pin=4,active_high=True,initial_value=False)
+        time.sleep(1)
+        self.enable_pin.on()
+        time.sleep(1)
+
+        CST_LSS_PORT = "/dev/ttyAMA0"
+        CST_LSS_BAUD = lssc.LSS_DefaultBaud
+        lss.initBus(CST_LSS_PORT, CST_LSS_BAUD)
+
+        self.servo_one = lss.LSS(1)
+        self.servo_two = lss.LSS(2)
+        self.servo_three = lss.LSS(3)
+
+        self.axis5 = 0.0
+        self.axis6 = 0.0
+
+        self.homing_service = rospy.Service("/home_end_effector", HomeArmBase, self.home_end_effector)
         self.homing_in_progress = False
         self.homing_success = False
 
-        self.enable_service = rospy.Service("/enable_actuators", EnableActuators, self.enable_actuators)
-        self.actuators_enabled = False
-        self.enable_pin = gpiozero.OutputDevice(pin=4,active_high=True,initial_value=False)
+    def process_rate_cmd(self, rate_cmd):
+        #self.ser.write(str(rate_cmd.axis) + ',' + str(rate_cmd.desiredRate) + "\n")        
+        if rate_cmd.axis == 6:   # roll
+            self.axis6 = rate_cmd.desiredRate
+        elif rate_cmd.axis == 5: # yaw
+            self.axis5 = rate_cmd.desiredRate
+        else:
+            return
 
-        self.topic_publisher_callback = {
-            'status' : self.process_status,
-            'feedback' : self.process_feedback,
-            'response': self.process_status,
-            'error': self.process_status,
-            'homing_status' : self.process_homing_status
-        }
+        # s 2 = yaw + roll
+        # s 3 = -yaw + roll
+        self.servo_two.wheel(int(50*(self.axis6 + self.axis5)))
+        self.servo_three.wheel(int(50*(self.axis6 - self.axis5)))
 
-
-    def home_arm_base(self, home_srv):
+    def home_end_effector(self, home_srv):
         if not self.homing_in_progress and not self.homing_success:
             self.ser.write('h 0\n')
             self.homing_in_progress = True
@@ -61,8 +71,7 @@ class ArmRelay:
         elif "true" in status:
             self.homing_success = True
 
-    def enable_actuators(self,enable_srv):
-        
+    def enable_servos(self,enable_srv):        
         if self.actuators_enabled and not enable_srv.enable:
             self.enable_pin.off()
             rospy.loginfo('Disabling Actuators')
@@ -72,15 +81,11 @@ class ArmRelay:
 
         self.actuators_enabled = self.enable_pin.value
         return EnableActuatorsResponse(self.actuators_enabled)
-
-    def process_rate_cmd(self, rate_cmd):
-        self.ser.write(str(rate_cmd.axis) + ',' + str(rate_cmd.desiredRate) + "\n")
         
     def process_status(self, data):
         self.status_pub.publish(str(data))
         
-    def process_feedback(self, data):
-        
+    def process_feedback(self, data):        
         actuatorFeedback = ActuatorFeedback()
         
         # parse input data into keys for reference
@@ -95,27 +100,15 @@ class ArmRelay:
         actuatorFeedback.actualRate = float(results['r'])
         
         self.feedback_pub.publish(actuatorFeedback)
-        
-    def process_message(self, message):
-        args = message.split(";")
-        topic = args[0]
-        data = args[1]
-        try:
-            self.topic_publisher_callback[topic](data)
-        except Exception as e:
-            print(e)
+
     
     def run(self):
         rate = rospy.Rate(60)
         while not rospy.is_shutdown():
-            while self.ser.in_waiting > 0:
-                message = self.ser.read_until('\n')
-                print(message)
-                self.process_message(message)
             rate.sleep()
 
 
 if __name__ == '__main__':
-    relay = ArmRelay()
+    relay = EndEffectorController()
     relay.run()
   
