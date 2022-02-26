be8ab217b5590fc782f24194d563d10a1a3a7c62
Areeb-M <am0247@uah.edu>
2022-02-25 20:20:48 -0600

    Code finally works

--- src/relay.py
+++ src/relay.py
@@ -4,7 +4,7 @@ import rospy
 import serial
 import time
 from arm_relay.msg import JointRateCommand, ActuatorFeedback
-from arm_relay.src import HomeArmBase
+from arm_relay.srv import HomeArmBase, HomeArmBaseResponse
 from std_msgs import *
 
 class ArmRelay:
@@ -15,9 +15,9 @@ class ArmRelay:
         # Initialize serial port and connect to teensy microcontroller
         self.ser = None
         while (self.ser is None):
-            port = rospy.get_param('~arm_teensy_serial_port', '/dev/serial0')
+            port = rospy.get_param('~arm_teensy_serial_port', '/dev/ttyACM0')
             try:
-                self.ser = serial.Serial(port,115200)
+                self.ser = serial.Serial(port,9600)
             except:
                 rospy.loginfo("Unable to locate Teensy (@{0}), please ensure communications.".format(port))
                 time.sleep(3)
@@ -29,7 +29,7 @@ class ArmRelay:
         self.cmd_rate_sub = rospy.Subscriber("/joint_rate_command", JointRateCommand, self.process_rate_cmd)
         
         self.homing_service = rospy.Service("/home_arm_base", HomeArmBase, self.home_arm_base)
-        self.homing_block = False
+        self.homing_in_progress = False
         self.homing_success = False
 
         self.topic_publisher_callback = {
@@ -37,24 +37,19 @@ class ArmRelay:
             'feedback' : self.process_feedback,
             'response': self.process_status,
             'error': self.process_status,
-            'homing_status' : self.process_homing
+            'homing_status' : self.process_homing_status
         }
 
 
     def home_arm_base(self, home_srv):
-        self.homing_block = True
-        self.homing_success = False
-
-        while self.homing_block:
-            while self.ser.in_waiting > 0:
-                message = self.ser.read_until('\n')
-                print(message)
-                self.process_message(message)
+        if not self.homing_in_progress and not self.homing_success:
+            self.ser.write('h 0\n')
+            self.homing_in_progress = True
         
-        return HomeArmBase(self.homing_success, self.homing_status)
+        return HomeArmBaseResponse(self.homing_success)
 
     def process_homing_status(self, status):
-        self.homing_block = False
+        self.homing_in_progress = False
         
         if status == "false":
             self.homing_success = False
@@ -88,8 +83,10 @@ class ArmRelay:
         args = message.split(";")
         topic = args[0]
         data = args[1]
-
-        self.topic_publisher_callback[topic](data)
+        try:
+            self.topic_publisher_callback[topic](data)
+        except Exception as e:
+            print(e)
     
     def run(self):
         rate = rospy.Rate(60)
