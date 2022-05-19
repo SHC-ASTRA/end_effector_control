7ddcfe507d5994d69f875b0328b2263a6739a891
Thomas <trb0023@uah.edu>
2022-05-18 19:44:32 -0500

    Adding functionality for new basestation

--- src/relay.py
+++ src/relay.py
@@ -5,7 +5,8 @@ import serial
 import time
 import gpiozero
 from arm_relay.msg import JointRateCommand, ActuatorFeedback
-from endeffector_controller.srv import EnableServos, EnableServosResponse, Home, HomeResponse
+from endeffector_controller.msg import EEStatus
+from endeffector_controller.srv import EnableServos, EnableServosResponse, Home, HomeResponse, EnableLaser, EnableLaserResponse, ZeroAxis, ZeroAxisResponse
 from std_msgs import *
 import lss
 import lss_const as lssc
@@ -17,6 +18,11 @@ class EndEffectorController:
 
         self.yaw_pos = 0
         self.roll_pos = 0
+        self.grip_pos = 0
+
+        self.yaw_zero = 0
+        self.roll_zero = 0
+        self.grip_zero = 0
 
         self.enable_service = rospy.Service("/enable_servos", EnableServos, self.enable_servos)
         self.enable_pin = gpiozero.OutputDevice(pin=4,active_high=True,initial_value=False)
@@ -60,6 +66,8 @@ class EndEffectorController:
         self.homing_in_progress = False
         self.homing_success = False
 
+        self.zero_service = rospy.Service("/zero_actuators", ZeroAxis, self.zero_axis)
+
     def process_rate_cmd(self, rate_cmd):
         #self.ser.write(str(rate_cmd.axis) + ',' + str(rate_cmd.desiredRate) + "\n")        
         if rate_cmd.axis == 6:   # roll
@@ -76,9 +84,22 @@ class EndEffectorController:
 
         # s 2 = yaw - roll
         # s 3 = -yaw - roll
-        self.servo_two.wheel(int(10*(-self.axis6 + self.axis5)))
-        self.servo_three.wheel(int(10*(-self.axis6 - self.axis5)))
+        self.servo_two.wheel(int(1*(-self.axis6*3 + self.axis5)))
+        self.servo_three.wheel(int(1*(-self.axis6*3 - self.axis5)))
         self.servo_one.wheel(int(10*self.axis7))
+
+    def zero_axis(self, zero_srv):
+        if zero_srv.axis == 5:
+            self.yaw_zero = self.yaw_pos + self.yaw_zero
+            return ZeroAxisResponse(True)
+        elif zero_srv.axis == 6:
+            self.roll_zero = self.roll_pos + self.roll_zero
+            return ZeroAxisResponse(True)
+        elif zero_srv.axis == 7:
+            self.grip_zero = self.grip_pos + self.grip_zero
+            return ZeroAxisResponse(True)
+        else:
+            return ZeroAxisResponse(False)
 
     def home_end_effector(self, home_srv):
         rospy.loginfo('Homing not implemented')
@@ -121,13 +142,16 @@ class EndEffectorController:
             pos2 = float(self.servo_two.getPosition())/10.0 - self.pos2off
             pos3 = float(self.servo_three.getPosition())/10.0 - self.pos3off
 
-            self.yaw_pos = (pos2/2.0 - pos3/2.0)
-            self.roll_pos = (-pos2/2.0 - pos3/2.0) / 3.0
+            self.yaw_pos = (pos2/2.0 - pos3/2.0) - self.yaw_zero
+            self.roll_pos = (-pos2/2.0 - pos3/2.0) / 3.0 - self.roll_zero
+            self.grip_pos = float(self.servo_three.getPosition())/10.0 - self.grip_zero
 
             #print(pos2,pos3,self.yaw_pos,self.roll_pos)
 
             self.process_feedback(5,self.yaw_pos,0)
             self.process_feedback(6,self.roll_pos,0)
+            self.process_feedback(7,self.grip_pos,0)
+            
         except:
             rospy.loginfo("Servo failed to return feedback")
         
